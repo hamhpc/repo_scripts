8e83fc4dfca29bd4202ac16c64f020fbabb7fd74
Steve Young <slyoung@hamilton.edu>
2018-03-28 14:58:47 -0400

    Update install_requirement_packages_on_server.sh

--- bin/install_requirement_packages_on_server.sh
+++ bin/install_requirement_packages_on_server.sh
@@ -1,10 +1,5 @@
-#yum -y install gcc
-#yum -y install gcc-c++
-#yum -y install autoconf
-#yum -y install java-1.6.0-openjdk.x86_64
-#yum -y install ImageMagick
+yum -y install ImageMagick
 yum -y install ghostscript
-#yum -y install ant
 yum -y install git
 yum -y install tesseract
 yum -y install leptonica
@@ -13,7 +8,17 @@ yum -y install leptonica
 # ffmpeg2theora
 #
 wget -N -P /usr/local/src http://v2v.cc/~j/ffmpeg2theora/ffmpeg2theora-0.29.linux64.bin
-install -m 755 /usr/local/src/ffmpeg2theora-0.29.linux64.bin /usr/bin/ffmpeg2theora
+install -m 755 /usr/local/src/ffmpeg2theora-0.29.linux64.bin /usr/local/bin/ffmpeg2theora
+
+#
+## Dependancy software installation of fits.sh
+#
+wget -N -P /usr/local/src https://fits.googlecode.com/files/fits-0.6.2.zip
+cd /usr/local/src
+unzip fits-0.6.2.zip
+cd fits-0.6.2
+./configure --prefix=/usr/local/fits-0.6.2
+make install
 
 # link fits.sh 
-ln -s $ISLANDORA_HOME/packages/fits-0.6.2/fits.sh $ISLANDORA_HOME/bin/fits.sh
+ln -s /usr/local/fits-0.6.2/fits.sh /usr/local/bin/fits.sh
